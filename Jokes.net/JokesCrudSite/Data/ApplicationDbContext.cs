520cbef956b9bdcedd92726408549ba15c067609
doty1130 <57019476+doty1130@users.noreply.github.com>
2021-10-03 23:06:37 -0400

    Controllers and Data files
    Upload 3

---
+++ Jokes.net/JokesCrudSite/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JokesCrudSite.Models;
+
+namespace JokesCrudSite.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesCrudSite.Models.Joke> Joke { get; set; }
+    }
+}
