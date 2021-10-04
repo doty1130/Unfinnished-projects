7e34ddfabae40f28b6edeba484b7adaae8d7de5b
doty1130 <57019476+doty1130@users.noreply.github.com>
2021-10-03 22:59:38 -0400

    Http Server
    Added in other repos. 
    Just placed here as proof that i do stuff

---
+++ HttpsServer/HttpsServer/bin/Debug/netcoreapp3.1/root/web/Javascript/myscripts.js
@@ -0,0 +1,14 @@
+var imageArray = "url('Photos/no-file.png')";
+var cards = document.querySelectorAll("[id = 'card']"); 
+
+
+cards[0].style.backgroundImage = imageArray;
+cards[1].style.backgroundImage = imageArray;
+cards[2].style.backgroundImage = imageArray;
+cards[3].style.backgroundImage = imageArray;
+cards[4].style.backgroundImage = imageArray;
+
+function SetCard()
+{
+	
+}
